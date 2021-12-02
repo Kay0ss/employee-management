98bfb8630617c9fac9249bdcaca3d62bcf728209
Jack <85977030+Kay0ss@users.noreply.github.com>
2021-12-02 14:40:53 -0600

    updated view all employees database query to match output specified in server.js

--- db/query.sql
+++ db/query.sql
@@ -3,6 +3,14 @@
 -- View all employees --
 SELECT employee.id, first_name, last_name, title, department.name AS department, salary, manager_id
 FROM employeeJOIN role ON employee.role_id = rolew.idJOIN department ON role.department_id = department.id;
+JOIN role ON employee.role_id = role.id
+JOIN department ON role.department_id = department.id;
+
+SELECT employee.id, employee.first_name, employee.last_name, title, department.name AS department, salary, CONCAT(e.first_name, ' ', e.last_name) AS manager
+FROM employee
+JOIN role ON employee.role_id = role.id
+JOIN department ON role.department_id = department.id
+LEFT JOIN employee e ON employee.manager_id = e.id;
 
 --View all managers with ids--
 SELECT id, first_name, last_name FROM employee WHERE manager_is NULL;
@@ -34,4 +42,3 @@ SELECT * department;
 INSERT INTO department (name)
     VALUES
         (name);
-        
